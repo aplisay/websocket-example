f8d570d6af6c3f8b45ca21fdb9b269f6fc4a0705
Rob Pickering <rob@pickering.org>
2025-04-09 22:30:11 +0100

    production url

--- websocket-test.js
+++ websocket-test.js
@@ -25,7 +25,7 @@ let microphone = new Mic({
 
 
 // Base URL for the API
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://llm-agent.aplisay.com/api';
 
 // Hardcode the API key temporarily for testing
 const { API_KEY } = process.env;
